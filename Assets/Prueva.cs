242c14b8d94d0ebe4a24df2135e8f4ad8b7d923d
Piedra <piedraj328@gmail.com>
2019-12-09 17:22:59 -0600

    Eduardo cambio 1

---
+++ Assets/Prueva.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueva : MonoBehaviour
+{
+    float[] array;
+    bool test = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        array = new float[10];
+        for(int i = 0; i < array.Length; i++)
+        {
+            array[0] = i * 10;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (array.Length > 5) test = true;
+    }
+}
